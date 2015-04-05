825d3a07bf492b039e299cb15894149ca4202e46
sanha <sanhaleehana@naver.com>
2015-04-05 22:24:57 +0900

    ASS4 little

--- sf/Assignment04.v
+++ sf/Assignment04.v
@@ -103,36 +103,34 @@ Fixpoint fold {X Y:Type} (f: X->Y->Y) (l:list X) (b:Y) : Y :=
     and complete the proofs below. *)
 
 Fixpoint repeat {X : Type} (n : X) (count : nat) : list X :=
-  (* FILL IN HERE *) admit.
+  match count with 
+  | O => nil
+  | S count' => cons n (repeat n count')
+  end.
 
 Example test_repeat1:
   repeat true 2 = cons true (cons true nil).
- (* FILL IN HERE *) Admitted.
+Proof.
+  simpl. reflexivity.
+Qed.
 
 Theorem nil_app : forall X:Type, forall l:list X,
   app [] l = l.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X l. destruct l.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed. 
 
 Theorem snoc_with_append : forall X : Type,
                          forall l1 l2 : list X,
                          forall v : X,
   snoc (l1 ++ l2) v = l1 ++ (snoc l2 v).
 Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-
-
-
-
-
-
-
-
-
-
-
+  intros X l1 l2 v. induction l1 as [| l1'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHl1. reflexivity.
+Qed.
 
 
 
