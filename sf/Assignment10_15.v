157a674186d12b76857923b4fbd77c6ebb17b41e
sanha <sanhaleehana@naver.com>
2015-05-31 03:50:29 +0900

    HW10 completed

--- sf/Assignment10_15.v
+++ sf/Assignment10_15.v
@@ -22,10 +22,63 @@ Proof.
   induction b ; intros.
   - left. eauto.
   - left. eauto.
-  - right. 
-
-
-  exact FILL_IN_HERE.
+  - right. destruct a. 
+    + destruct a0 ;  eauto.
+      * assert ((exists n, APlus a0_1 a0_2 = ANum n) \/ (exists a', APlus a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_Eq2. eauto. eauto.
+      * assert ((exists n, AMinus a0_1 a0_2 = ANum n) \/ (exists a', AMinus a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_Eq2. eauto. eauto.
+      * assert ((exists n, AMult a0_1 a0_2 = ANum n) \/ (exists a', AMult a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_Eq2. eauto. eauto.
+    + assert ((exists n, AId i = ANum n) \/ (exists a', AId i  / st ==>a a')). apply aexp_strong_progress.
+      eexists. eauto. 
+    + assert ((exists n, APlus a1 a2 = ANum n) \/ (exists a', APlus a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+    + assert ((exists n, AMinus a1 a2 = ANum n) \/ (exists a', AMinus a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+    + assert ((exists n, AMult a1 a2 = ANum n) \/ (exists a', AMult a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+  - right. destruct a. 
+    + destruct a0 ;  eauto.
+      * assert ((exists n, APlus a0_1 a0_2 = ANum n) \/ (exists a', APlus a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_LtEq2. eauto. eauto.
+      * assert ((exists n, AMinus a0_1 a0_2 = ANum n) \/ (exists a', AMinus a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_LtEq2. eauto. eauto.
+      * assert ((exists n, AMult a0_1 a0_2 = ANum n) \/ (exists a', AMult a0_1 a0_2 / st ==>a a')).
+        apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0. 
+        eexists. apply BS_LtEq2. eauto. eauto.
+    + assert ((exists n, AId i = ANum n) \/ (exists a', AId i  / st ==>a a')). apply aexp_strong_progress.
+      eexists. eauto. 
+    + assert ((exists n, APlus a1 a2 = ANum n) \/ (exists a', APlus a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+    + assert ((exists n, AMinus a1 a2 = ANum n) \/ (exists a', AMinus a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+    + assert ((exists n, AMult a1 a2 = ANum n) \/ (exists a', AMult a1 a2  / st ==>a a')). apply aexp_strong_progress.
+      inversion H. inversion H0. inversion H1. inversion H0. eexists. eauto.
+  - right. destruct b.
+    + eexists; constructor.
+    + eexists; constructor.
+    + inversion IHb. inversion H. inversion H0. inversion H0. inversion H. eexists. eauto. 
+    + inversion IHb. inversion H. inversion H0. inversion H0. inversion H. eexists. eauto. 
+    + inversion IHb. inversion H. inversion H0. inversion H0. inversion H. eexists. eauto. 
+    + inversion IHb. inversion H. inversion H0. inversion H0. inversion H. eexists. eauto. 
+  - right. destruct b1. 
+    + destruct b2; eauto; try (inversion IHb2; inversion H; inversion H0; inversion H0; inversion H; 
+        eexists; eauto).
+    + destruct b2; eauto.
+    + inversion IHb1. inversion H. inversion H0. inversion H0. inversion H. 
+      eexists. eauto. 
+    + inversion IHb1. inversion H. inversion H0. inversion H0. inversion H. 
+      eexists. eauto.
+    + inversion IHb1. inversion H. inversion H0. inversion H0. inversion H. 
+      eexists. eauto.
+    + inversion IHb1. inversion H. inversion H0. inversion H0. inversion H. 
+      eexists. eauto.
 Qed.
 
 (*-- Check --*)
@@ -33,44 +86,4 @@ Check bexp_strong_progress: forall st b,
   (b = BTrue \/ b = BFalse) \/
   exists b', b / st ==>b b'.
 
-
-Theorem aexp_strong_progress: forall st a,
-  (exists n, a = ANum n) \/
-  exists a', a / st ==>a a'.
-Proof.
-  intros. induction a ; intros.
-  - left. eauto.
-  - right. eauto.
-  - right. destruct a1.
-    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
-        inversion H; eexists; apply AS_Plus2; eauto; eauto).
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      apply AS_Plus1. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      apply AS_Plus1. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      apply AS_Plus1. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      apply AS_Plus1. eauto.
-  - right. destruct a1.
-    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
-        inversion H; eexists; apply AS_Minus2; eauto; eauto).
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-  - right. destruct a1.
-    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
-        inversion H; eexists; apply AS_Mult2; eauto; eauto).
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
-    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
-      constructor. eauto.
+  
--- sf/Assignment10_16.v
+++ sf/Assignment10_16.v
@@ -13,15 +13,53 @@ Require Export Assignment10_15.
      destruct ... as [ | [? [? ?]]].
 *)
 
+Hint Constructors cstep.
+
 Theorem cimp_strong_progress : forall c st,
   c = SKIP \/ 
   exists c' st', c / st ==>c c' / st'.
 Proof.
-  exact FILL_IN_HERE.
+  intros. induction c.
+  - left. reflexivity.
+  - right. destruct a ; eauto. 
+    + assert ((exists n, APlus a1 a2 = ANum n) \/ (exists a', APlus a1 a2 / st ==>a a')).
+      apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0.
+      eexists. eauto.
+    + assert ((exists n, AMinus a1 a2 = ANum n) \/ (exists a', AMinus a1 a2 / st ==>a a')).
+      apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0.
+      eexists. eauto.
+    + assert ((exists n, AMult a1 a2 = ANum n) \/ (exists a', AMult a1 a2 / st ==>a a')).
+      apply aexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H0.
+      eexists. eauto.  
+  - right. destruct c1; eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists. eexists. eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists. eexists. eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists. eexists. eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists. eexists. eauto.
+  - right. destruct b ; eauto.
+    + assert ((BEq a a0 = BTrue \/ BEq a a0 = BFalse) \/ exists b', BEq a a0 / st ==>b b').
+      apply bexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H1. inversion H0.
+      eexists. eexists. eauto.
+    + assert ((BLe a a0= BTrue \/ BLe a a0 = BFalse) \/ exists b', BLe a a0 / st ==>b b').
+      apply bexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H1. inversion H0.
+      eexists. eexists. eauto.
+    + assert ((BNot b = BTrue \/ BNot b = BFalse) \/ exists b', BNot b / st ==>b b').
+      apply bexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H1. inversion H0.
+      eexists. eexists. eauto.
+    + assert ((BAnd b1 b2 = BTrue \/ BAnd b1 b2 = BFalse) \/ exists b', BAnd b1 b2/ st ==>b b').
+      apply bexp_strong_progress. inversion H. inversion H0. inversion H1. inversion H1. inversion H0.
+      eexists. eexists. eauto.
+  - right. destruct b ; eauto.
+  - right. destruct c1 ; eauto.
+    + inversion IHc2. rewrite H. eexists. eauto. inversion H. inversion H0.
+      eexists. eexists. eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists.  eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists.  eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists.  eauto.
+    + inversion IHc1. inversion H. inversion H. inversion H0. eexists.  eauto. 
 Qed.
 
 (*-- Check --*)
 Check cimp_strong_progress : forall c st,
   c = SKIP \/ 
   exists c' st', c / st ==>c c' / st'.
-
