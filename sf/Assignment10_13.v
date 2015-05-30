19ef8637b45dbfc2c4264757fd979df77a13e9b0
sanha <sanhaleehana@naver.com>
2015-05-30 23:58:46 +0900

    HW10 12~15

--- sf/Assignment10_13.v
+++ sf/Assignment10_13.v
@@ -8,7 +8,14 @@ Lemma par_body_n : forall n st,
   exists st',
     par_loop / st ==>c*  par_loop / st' /\ st' X = n /\ st' Y = 0.
 Proof.
-  exact FILL_IN_HERE.
+  intros. induction n.
+  { exists st. split. constructor. assumption. }
+  { inversion IHn. inversion H0. apply par_body_n__Sn in H2.
+    exists (update x X (S n)). split.
+    apply multi_trans with (y := (par_loop, x)) ; assumption.
+    split. compute. reflexivity. inversion H0. inversion H4.
+    unfold update. destruct (eq_id_dec X Y). solve by inversion. assumption.
+  } 
 Qed.
 
 (*-- Check --*)
--- sf/Assignment10_14.v
+++ sf/Assignment10_14.v
@@ -14,11 +14,49 @@ Require Export Assignment10_13.
      destruct ... as [[? ?] | [? ?]].
 *)
 
+Hint Constructors aval.
+Hint Constructors astep.
+
 Theorem aexp_strong_progress: forall st a,
   (exists n, a = ANum n) \/
   exists a', a / st ==>a a'.
 Proof.
-  exact FILL_IN_HERE.
+  intros. induction a ; intros.
+  - left. eauto.
+  - right. eauto.
+  - right. destruct a1.
+    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
+        inversion H; eexists; apply AS_Plus2; eauto; eauto).
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      apply AS_Plus1. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      apply AS_Plus1. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      apply AS_Plus1. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      apply AS_Plus1. eauto.
+  - right. destruct a1.
+    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
+        inversion H; eexists; apply AS_Minus2; eauto; eauto).
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+  - right. destruct a1.
+    + destruct a2 ; eauto ; try (inversion IHa2; inversion H; inversion H0;
+        inversion H; eexists; apply AS_Mult2; eauto; eauto).
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
+    + inversion IHa1. inversion H. inversion H0. inversion H. eexists. 
+      constructor. eauto.
 Qed.
 
 (*-- Check --*)
